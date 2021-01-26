d689b8f574a1d47407380045fcdbd69ff00598ee
MichellePonti <michelle.ponti@heig-vd.ch>
2021-01-26 16:45:55 +0100

    first commit with tabs

---
+++ Spotless/src/app/layout/layout-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LayoutPage } from './layout.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPage,
+    children: [
+      {
+
+        path: 'liste',
+        loadChildren: () => import('./liste/liste.module').then(m => m.ListePageModule)
+      },
+      {
+        path: 'carte',
+        loadChildren: () => import('./carte/carte.module').then(m => m.CartePageModule)
+      },
+      {
+        path: 'compte',
+        loadChildren: () => import('./compte/compte.module').then(m => m.ComptePageModule)
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class LayoutPageRoutingModule { }
