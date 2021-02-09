d084b4c1d6642a1b15e9d3485fc5ca34882657ed
Mikaela Monachon <mikaela.monachon@heig-vd.com>
2021-02-09 19:49:08 +0100

    auth

--- Spotless/src/app/app.module.ts
+++ Spotless/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
@@ -22,7 +21,6 @@ import { IonicStorageModule } from "@ionic/storage";
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, IonicStorageModule.forRoot()],
   providers: [
     StatusBar,
-    SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
