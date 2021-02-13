6a5b98c3104a017de24207da29921e7334689794
Mikaela Monachon <mikaela.monachon@heig-vd.com>
2021-02-13 15:23:21 +0100

    register

--- Spotless/src/app/app.component.ts
+++ Spotless/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-// import { Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Place } from './models/place';
 
 @Component({
